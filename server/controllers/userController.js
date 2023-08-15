b9997cc2e7b4af1e602038197f1cae1a3e3bf3bc
Yogit <you@example.com>
2023-08-15 16:38:25 +0530

    Signup frontend complete, design modifications req

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,11 +33,10 @@ exports.createUser = async (req, res) => {
   try {
 
     const existingUser = await User.findOne({ useremail });
+    console.log(existingUser);
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
-
-
 
     const newUser = new User(req.body);
     await newUser.save();
